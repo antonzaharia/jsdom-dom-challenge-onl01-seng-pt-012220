c1df5a626ff5928f385cb97e5f35c00ec7d4001b
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-22 15:40:44 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -7,4 +7,5 @@ document.getElementById("submit").addEventListener('click', function(event){
   let comments = document.getElementById('list');
   comments.appendChild(p);
   event.preventDefault()
+  input.reset();
 });
