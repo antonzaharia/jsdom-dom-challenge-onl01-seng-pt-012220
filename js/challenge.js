7b6e25ba6b25e618db4d6c9435de0456875eacdf
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-22 16:56:32 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -61,7 +61,7 @@ like.addEventListener('click', function(event){
   function addLike(){
     return likes + 1;
   }
-  addlike();
+  addLike();
   let p = document.createElement('p');
   p.textContent = `${counter.textContent} has ${likes} likes`;
   console.log(p);
