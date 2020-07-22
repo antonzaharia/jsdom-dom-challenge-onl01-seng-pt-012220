06ca668506d205f340473291e7a914cf84870b58
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-07-22 15:35:17 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -4,6 +4,6 @@ document.getElementById("submit").addEventListener('click', function(event){
   let p = document.createElement('p');
   let newComment = p.createTextNode(input);
   let comments = document.getElementById('list');
-  comment.appendChild()
+  comments.appendChild()
   event.preventDefault()
 });
